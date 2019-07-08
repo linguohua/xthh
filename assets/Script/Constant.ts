82f03580c1dc9b69e30be8d7cce07e42c59c6543
riguang <zscboy@gmail.com>
2019-07-08 19:36:14 +0800

    修改版本号

--- assets/Script/Constant.ts
+++ assets/Script/Constant.ts
@@ -1,12 +1,3 @@
-export type EnvType = "develop" | "test" | "prepublish" | "online";
-/**
- * Author:  王冬春
- * Version: 1.0
- * Description: 常量声明
- * History: 无
- */
 export namespace Constant {
-    export const gameVersion: string = "1.0.0.0629"; //服务器版本号
-    export const env: EnvType = "test"; //业务环境
-    export const debug: boolean = false; //调试开关
+    export const gameVersion: string = "1.0.0.1907081932"; //服务器版本号
 }
