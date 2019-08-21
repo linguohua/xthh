9f24eea4132a43b9cfabede69abb6cbfde9b7eb2
riguang <zscboy@gmail.com>
2019-08-21 14:20:50 +0800

    版本号

--- assets/Script/modules/lobby/lcore/LEnv.ts
+++ assets/Script/modules/lobby/lcore/LEnv.ts
@@ -2,7 +2,7 @@
  * lobby 配置
  */
 export namespace LEnv {
-    export const VER_STR: string = "v1.0.082003";
+    export const VER_STR: string = "v1.0.082101";
 
     // updateQuery = "/lobby/upgrade/query",
     export const updateQuery: string = "/lobby/uuid/upgradeQuery";
