8afc1e3feceb3d17818605c790897ce2b6612185
riguang <zscboy@gmail.com>
2019-09-27 10:42:22 +0800

    商店支付

--- assets/Script/modules/lobby/lcore/LEnv.ts
+++ assets/Script/modules/lobby/lcore/LEnv.ts
@@ -68,6 +68,11 @@ export namespace LEnv {
     export const baiduMapApi = "https://api.map.baidu.com/reverse_geocoding/v3/?&location=";
     export const yunxinAppKey = "f31690906e1428b3a6a1c9147580b3b7";
     export const app = "h5casino";
+    export const offerID = "1450022179";
+    // 米大师支付模式，0是沙箱，1是正式环境
+    export const isDebug = true;
+
+    export const shipmentsUrl = "/zyqp/pay/payOrder/{0}/{1}/{2}";
 
     // tslint:disable-next-line:no-http-string
     export const wxLogin = "/zyqp/user/h5login/withcode?app={0}&code={1}";
